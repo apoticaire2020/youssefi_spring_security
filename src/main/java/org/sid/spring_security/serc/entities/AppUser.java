5f38ab3d918f5d39791d4a1ce53fb6403f3c7902
apoticaire2020 <andalossi2008@gmail.com>
2023-03-30 16:18:14 +0000

    controller suite

--- src/main/java/org/sid/spring_security/serc/entities/AppUser.java
+++ src/main/java/org/sid/spring_security/serc/entities/AppUser.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 
 import javax.persistence.*;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Entity
@@ -17,6 +18,7 @@ public class AppUser {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id ;
 	private String userName;
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	private String password;
 	@ManyToMany(fetch = FetchType.EAGER)
 	private Collection<AppRole> appRoles = new ArrayList<>();
--- src/main/java/org/sid/spring_security/serc/web/AccountRestController.java
+++ src/main/java/org/sid/spring_security/serc/web/AccountRestController.java
@@ -1,8 +1,12 @@
 package org.sid.spring_security.serc.web;
 
+import lombok.Data;
+import org.sid.spring_security.serc.entities.AppRole;
 import org.sid.spring_security.serc.entities.AppUser;
 import org.sid.spring_security.serc.service.AccountService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,8 +18,25 @@ public class AccountRestController {
     public AccountRestController(AccountService service) {
         this.service = service;
     }
-@GetMapping(path = "/users")
+     @GetMapping(path = "/users")
     public List<AppUser> userList(){
        return service.listUsers();
     }
+    @PostMapping(path = "/users")
+    public  AppUser saveUser(@RequestBody AppUser user){
+        return  service.addNewUser(user);
+    }
+    @PostMapping(path = "/roles")
+    public AppRole saveUser(@RequestBody AppRole role){
+        return  service.addNewRole(role);
+    }
+    @PostMapping(path = "/addRoleToUser")
+    public void addRoleToUser(@RequestBody RoleUserForm roleUserForm){
+          service.addRoleToUser(roleUserForm.getUsername(),roleUserForm.getRoleName());
+    }
+}
+@Data
+class RoleUserForm{
+    private String username;
+    private  String roleName;
 }
