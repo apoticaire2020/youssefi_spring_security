806e713674917c512fe31bf3954c2298c95d1ad3
apoticaire2020 <andalossi2008@gmail.com>
2023-04-06 12:15:22 +0000

    token generet

--- src/main/java/org/sid/spring_security/serc/filters/JwAuthenticationFilter.java
+++ src/main/java/org/sid/spring_security/serc/filters/JwAuthenticationFilter.java
@@ -1,8 +1,12 @@
 package org.sid.spring_security.serc.filters;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -10,6 +14,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 public class JwAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -21,11 +27,28 @@ public class JwAuthenticationFilter extends UsernamePasswordAuthenticationFilter
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+        System.out.println("attemptAuthentication");
+         String username = request.getParameter("username");
+         String password = request.getParameter("password");
+        System.out.println(username);
+        System.out.println(password);
+        UsernamePasswordAuthenticationToken authenticationToken=
+                new UsernamePasswordAuthenticationToken(username,password);
+        return authenticationManager.authenticate(authenticationToken);
     }
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+        System.out.println("successfulAuthentication");
+        User user = (User) authResult.getPrincipal();
+        Algorithm algo1 = Algorithm.HMAC256("sos");
+        String jwtAccessToken = JWT.create()
+                .withSubject(user.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis()+5*60*1000))
+                .withIssuer(request.getRequestURI().toString())
+                .withClaim("roles" ,user.getAuthorities()
+                                        .stream().map(ga-> ga.getAuthority() ).collect(Collectors.toList()))
+                .sign(algo1);
+        response.setHeader("Authorization" , jwtAccessToken);
     }
 }
